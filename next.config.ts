36cb1b449d1a15f4aef995fef92ca610ef07f7cb
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-08-18 18:46:21 +0530

    fix: webcontainer preview fixed, webcontainer origin header added to next-config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,23 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
